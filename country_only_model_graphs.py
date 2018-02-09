af69166af49efc4220e9494b0c5e757b5843ab8b
angwhen <angelmwen@gmail.com>
2018-02-09 11:51:52 -0500

    graph for english spanish

--- country_only_model_graphs.py
+++ country_only_model_graphs.py
@@ -18,6 +18,19 @@ def graph_english_chinese_1950_to_2050(lang_pop_dict):
     ax.set_xlabel("Year")
     plt.show()
 
+def graph_english_spanish_1950_to_2050(lang_pop_dict):
+    english_speakers = lang_pop_dict["English"]
+    spanish_speakers = lang_pop_dict["Spanish"]
+
+    fig, ax = plt.subplots()
+    ax.set_title("Spanish and English Speakers")
+    ax.plot(xrange(1950,2055,5),english_speakers,"b")
+    ax.plot(xrange(1950,2055,5),spanish_speakers,"g")
+    ax.set_xlim(1950,2050)
+    ax.set_ylabel("Speakers in Thousands")
+    ax.set_xlabel("Year")
+    plt.show()
+    
 
 # this dict has each key as a language name
 # and each value is an array
@@ -25,4 +38,5 @@ def graph_english_chinese_1950_to_2050(lang_pop_dict):
 # and the last index (20) being the population projected at 2050
 lang_pop_dict = pickle.load(open("lang_pop_dict.p","rb"))
 print lang_pop_dict.keys()
-graph_english_chinese_1950_to_2050(lang_pop_dict)
+#graph_english_chinese_1950_to_2050(lang_pop_dict)
+graph_english_spanish_1950_to_2050(lang_pop_dict)
