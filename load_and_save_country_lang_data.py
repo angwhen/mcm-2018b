50e26eb70ba8684b078f2e8fa61f35555825351c
angwhen <angelmwen@gmail.com>
2018-02-09 11:06:16 -0500

    starting graph file

--- load_and_save_country_lang_data.py
+++ load_and_save_country_lang_data.py
@@ -6,8 +6,8 @@ data = get_data("country_languages.ods")["Sheet1"]
 
 country_lang_dict = {}
 for country in data:
-    country_name = str(country[0])
-    main_language = re.split('[^a-zA-Z]',country[1].encode('ascii','ignore'))[0]
+    country_name = str(country[0]).strip()
+    main_language = re.split('[^a-zA-Z]',country[1].encode('ascii','ignore'))[0].strip()
     print main_language
     country_lang_dict[country_name] = main_language
 
--- make_and_save_lang_pop_data.py
+++ make_and_save_lang_pop_data.py
@@ -4,9 +4,10 @@ countries_pop_dict = pickle.load(open("countries_pop_dict.p","rb"))
 countries_lang_dict = pickle.load(open("country_lang_dict.p","rb"))
 
 # make language to population/year dict
-lang_pop_dict = {}
+lang_pop_dict = {} # will have array for each language index 0 is 1950, and every consec index is 5 more years
 countries_with_pop = countries_pop_dict.keys()
 for country_name in countries_lang_dict.keys():
+    orig_country_name = country_name
     country_name = country_name.replace("St.","Saint")
     country_name = country_name.replace(" (proposed state)","")
     country_name = country_name.replace(" (proposed)","")
@@ -22,9 +23,17 @@ for country_name in countries_lang_dict.keys():
         elif country_name == "Palestinian State":
             country_pop_name = "State of Palestine"
         else:
+            print country_name
             continue # can't match
     else:
         country_pop_name = country_name
-    curr_lang = countries_lang_dict[country_name]
+    curr_lang = countries_lang_dict[orig_country_name]
     if curr_lang not in lang_pop_dict:
-        
+        lang_pop_dict[curr_lang] = [0]*21
+        for year in countries_pop_dict[country_pop_name].keys():
+            lang_pop_dict[curr_lang][(year-1950)/5] = countries_pop_dict[country_pop_name][year]
+    else:
+        for year in countries_pop_dict[country_pop_name].keys():
+            lang_pop_dict[curr_lang][(year-1950)/5] += countries_pop_dict[country_pop_name][year]
+            
+pickle.dump(lang_pop_dict, open("lang_pop_dict.p","wb"))
